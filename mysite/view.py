0f9297ea92143149330095f5b1a11e40a62f3ba4
lvasu123 <lvpuchao@gmail.com>
2019-02-25 20:23:37 +0800

    django test_program 1

---
+++ mysite/view.py
@@ -0,0 +1,6 @@
+def index(resquest):
+        return HttpResponse('this is index.')
+ 
+def hello(request):
+        return HttpResponse('Hello World')
+
---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    re_path(r'^$',views.index),
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
